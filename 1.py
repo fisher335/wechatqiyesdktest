fa6eddc9d9233947a3d869fce092753b84734237
fengshaomin <fengshaomin@bjsasc.com>
2018-05-02 17:53:54 +0800

    Initial commit

---
+++ 1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Date    : '2018/4/24 0024'
+# @Author  : Terry feng  (fengshaomin@qq.com)
+
+
+import json
+from io import StringIO
+
+a = StringIO()
+
+
+
+class test():
+    def getva(self):
+        return a.getvalue()
+
+
+if __name__ == '__main__':
+    a.getvalue()
+    d = json.loads("")
---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Date    : '2018/4/24 0024'
+# @Author  : Terry feng  (fengshaomin@qq.com)
+from wechat_sdk import *
+
+we = WeChatEnterprise()
+# 创建部门
+we.get_user("fengshaomin")
+# 其他方法类似， 注意是否有这个权限调用这些接口
